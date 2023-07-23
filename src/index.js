a9b1b23d75666bfc0309589fd43fe436bb0d9155
Ochuodho <ochuodhoderrick@gmail.com>
2023-07-23 10:23:57 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    handleToDo(e.target.new_task.value)
+    form.reset()
+
+  })
 });
+
+function handleToDo(todo){
+  let p = document.createElement('p')
+  p.textContent = `${todo} `
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+
+}
+
+
